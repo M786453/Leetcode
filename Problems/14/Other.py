e7f7b7d4c5c247e91f89853c38a4b48a835eb2e2
M786453 <ahteshamsarwar333@gmail.com>
2024-07-27 13:24:34 +0500

    Solution for problem#14.

---
+++ Problems/14/Other.py
@@ -0,0 +1,19 @@
+# Others, Fastest Solution
+
+from typing import List
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        ans=""
+        list=sorted(strs)
+        print("Sorted:", list)
+        first=list[0]
+        last=list[-1]
+
+        for i in range(min(len(first),len(last))):
+            if(first[i]!=last[i]):
+                return ans
+            ans+=first[i]
+        return ans
+    
+print(Solution().longestCommonPrefix(["flower","flow","flight"]))
