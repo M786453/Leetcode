d8e9a8036955753474f77815d70f3985bd6070cd
AHTESHAM SARWAR <57073399+M786453@users.noreply.github.com>
2025-05-04 23:54:29 +0500

    1071 Solved.

---
+++ Problems/1071.py
@@ -0,0 +1,28 @@
+# Greatest Common Divisor of String
+
+def gcd(str1, str2):
+    
+    length = len(str1)
+    
+    if length > len(str2):
+        
+        length = len(str2)
+        
+    for i in range(1,length+1):
+        
+        divisor = str1[:i]
+        
+        m1 = len(str1) // len(divisor)
+        
+        m2 = len(str2) // len(divisor)
+        
+        if divisor*m1 == str1 and divisor*m2 == str2:
+            return str1[:i]
+          
+    return ""
+    
+    
+print("Answer:", gcd("LEET","CODE"))
+    
+    
+    
