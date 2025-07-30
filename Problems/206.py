954a4a7f99596f9f9d3bda7a54f3e8d920e8c80f
M786453 <ahteshamsarwar333@gmail.com>
2025-07-30 13:46:14 +0500

    Add solution for 206.py

---
+++ Problems/206.py
@@ -0,0 +1,68 @@
+# Problem#206. Reverse Linked List
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def reverseList(self, head : ListNode):
+        # Time Complexity: Beats 100%
+        # Space Complexity: Beats 74.01%
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        
+        # Populate Stack
+
+        st = []
+
+        node = head
+
+        while node:            
+
+            st.append(node)
+
+            node = node.next
+
+        # Reverse linked list
+
+        head = None
+
+        node = None
+
+        while st:
+
+            if node:
+
+                node.next = st.pop()
+
+                node = node.next
+
+            else:
+
+                node = st.pop()
+
+            if not head:
+                head = node
+        
+        node.next = None # Tail of reverse list
+
+linked_list = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
+ss = Solution()
+
+ss.reverseList(linked_list)
+
+
+            
+
+            
+            
+
+
+
+
+
+        
