7569dfc0208538db0c3ea29d9a66286578c1df4c
M786453 <ahteshamsarwar333@gmail.com>
2025-07-30 13:59:08 +0500

    Update solution for 206.py

--- Problems/206.py
+++ Problems/206.py
@@ -50,8 +50,32 @@ class Solution(object):
         
         node.next = None # Tail of reverse list
 
+class Solution2(object):
+    def reverseList(self, head : ListNode):
+        # Time Complexity: Beats 100%
+        # Space Complexity: Beats 74.01%
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        
+        prev = None
+        curr = head
+        
+        while curr:
+
+            tmp = curr.next
+
+            curr.next = prev
+
+            prev = curr
+
+            curr = tmp
+
+        return curr
+
 linked_list = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
-ss = Solution()
+ss = Solution2()
 
 ss.reverseList(linked_list)
 
