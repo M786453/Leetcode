ab07b694266b762eab132d1823f18834cd5a1a50
M786453 <ahteshamsarwar333@gmail.com>
2025-07-31 12:48:01 +0500

    Update 206.py

--- Problems/206.py
+++ Problems/206.py
@@ -15,50 +15,6 @@ class Solution(object):
         :rtype: Optional[ListNode]
         """
         
-        # Populate Stack
-
-        st = []
-
-        node = head
-
-        while node:            
-
-            st.append(node)
-
-            node = node.next
-
-        # Reverse linked list
-
-        head = None
-
-        node = None
-
-        while st:
-
-            if node:
-
-                node.next = st.pop()
-
-                node = node.next
-
-            else:
-
-                node = st.pop()
-
-            if not head:
-                head = node
-        
-        node.next = None # Tail of reverse list
-
-class Solution2(object):
-    def reverseList(self, head : ListNode):
-        # Time Complexity: Beats 100%
-        # Space Complexity: Beats 74.01%
-        """
-        :type head: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        
         prev = None
         curr = head
         
@@ -74,10 +30,11 @@ class Solution2(object):
 
         return curr
 
-linked_list = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
-ss = Solution2()
+# linked_list = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
 
-ss.reverseList(linked_list)
+# ss = Solution()
+
+# ss.reverseList(linked_list)
 
 
             
