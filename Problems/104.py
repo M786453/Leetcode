8aee082adc6dd4d95272a67a27b1be310c5a0236
M786453 <ahteshamsarwar333@gmail.com>
2025-08-06 02:37:57 +0500

    Add solution for 104.py

---
+++ Problems/104.py
@@ -0,0 +1,34 @@
+# 104. Maximum Depth of Binary Tree
+
+class TreeNode:
+
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+    
+class Solution:
+
+    def maxDepth(self, root):
+
+        if root is None:
+            return 0
+        
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+    
+if __name__ == "__main__":
+
+    root = TreeNode(1)
+
+    root.left = TreeNode(9)
+    root.right = TreeNode(20)
+
+    root.left.left = TreeNode(15)
+    root.left.right = TreeNode(13)
+
+    root.right.right = TreeNode(6)
+    # root.right.right = TreeNode(7)
+
+    ss = Solution()
+
+    print("Answer:", ss.maxDepth(root))
