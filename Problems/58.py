64ec5f00d1f935a236623c11c56976b8a3e06b36
AHTESHAM SARWAR <ahteshamsarwar333@gmail.com>
2026-03-21 09:38:57 +0500

    Add 58.py updated solution

--- Problems/58.py
+++ Problems/58.py
@@ -6,21 +6,19 @@ class Solution(object):
         """
 
         current_word_length = 0
+        i = len(s)-1
 
-        for c in s[::-1]:
+        while i >= 0 and s[i] == " ":
+            i -= 1
 
-            if c == " ":
-                if current_word_length > 0:
-                    return current_word_length
-
-                continue
-
+        while i >= 0 and s[i] != " ":
             current_word_length += 1
-
+            i -= 1
+        
         return current_word_length
     
 s = Solution()
 
-# print("Answer:", s.lengthOfLastWord(s = "Hello World"))
+print("Answer:", s.lengthOfLastWord(s = "Hello World"))
 # print("Answer:", s.lengthOfLastWord("   fly me   to   the moon  "))
-print("Answer:", s.lengthOfLastWord("a"))
+# print("Answer:", s.lengthOfLastWord("a"))
